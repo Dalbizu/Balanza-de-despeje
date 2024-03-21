76377c3a9ca3f479c66fe7840bd0877c03155112
Dalbizu <70915352+Dalbizu@users.noreply.github.com>
2024-03-21 12:34:49 +0100

    Added angle functions to buttons
    Aparently they works perfectly

--- Scale.gd
+++ Scale.gd
@@ -1,6 +1,4 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	position = Vector2(get_viewport().size.x/2, get_viewport().size.y/3)
--- angle_scales.gd
+++ angle_scales.gd
@@ -1,45 +1,54 @@
 extends Node2D
+
+var rotating : bool = false
+@onready var left_plate : Node2D = get_node("Scale/LeftPlate")
+@onready var right_plate : Node2D = get_node("Scale/RightPlate")
+
+func rotate_scales(angle : float) -> void:
+	if angle >= $Scale.rotation:
+		rotate_scales_clockwise(angle)
+	elif angle <= $Scale.rotation:
+		rotate_scales_counterclockwise(angle)
+	else:
+		pass
+		
+func set_angle(angle : float) -> void:
+	$Scale.rotation = angle
+	get_node("Scale/LeftPlate").rotation = -angle
+	get_node("Scale/RightPlate").rotation = -angle
 	
 func rotate_scales_clockwise(angle : float) -> void:
 	var new_angle : float 
 	while(angle > $Scale.rotation):
-		print($Scale.rotation)
 		new_angle = lerp($Scale.rotation, angle, 0.05)
-		$Scale.rotation = new_angle
-		get_node("Scale/LeftPlate").rotation = -new_angle
-		get_node("Scale/RightPlate").rotation = -new_angle
+		set_angle(new_angle)
 		await get_tree().create_timer(0.01).timeout
 		if(new_angle >= angle - 0.005):
-			$Scale.rotation = angle
-	print("end")
+			set_angle(angle)
+			rotating = false
 		
-
 func rotate_scales_counterclockwise(angle : float) -> void:
 	var new_angle : float 
 	while(angle < $Scale.rotation):
 		new_angle = lerp($Scale.rotation, angle, 0.05)
-		$Scale.rotation = new_angle
-		get_node("Scale/LeftPlate").rotation = -new_angle
-		get_node("Scale/RightPlate").rotation = -new_angle
+		set_angle(new_angle)
 		await get_tree().create_timer(0.01).timeout
-
-func rotate_scales(angle : float) -> void:
-	if angle > $Scale.rotation:
-		rotate_scales_clockwise(angle)
-	elif angle < $Scale.rotation:
-		rotate_scales_counterclockwise(angle)
-	else:
-		pass
+		if(new_angle <= angle + 0.005):
+			set_angle(angle)
+			rotating = false
 
 func _on_to_left_button_pressed():
-	rotate_scales(-0.5)
-
+	if $Scale.rotation != -0.5 and not rotating:
+		rotate_scales(-0.5)
+		rotating = true
 
 func _on_to_center_button_pressed():
-	rotate_scales(0)
-
+	if $Scale.rotation != 0 and not rotating:
+		rotate_scales(0)
+		rotating = true
 
 func _on_to_right_button_pressed():
-	rotate_scales(0.5)
-	
-	pass # Replace with function body.
+	if $Scale.rotation != 0.5 and not rotating:
+		rotate_scales(0.5)
+		rotating = true
+
